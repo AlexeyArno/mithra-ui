ecd2ea5d9c525e68da043af39ae2f82e6ba6423d
Pestartz <iskanderbakhvalov1@gmail.com>
2018-09-23 19:49:12 +0300

    d

--- src/components/popular-screen/popular-topics/PopularTopics.tsx
+++ src/components/popular-screen/popular-topics/PopularTopics.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Link } from 'react-router-dom'
 import PopularTopicsTopBar from "./popular-topics-top-bar/PopularTopicsTopBar"
 import ContentTopics from "./content-topics/ContentTopics"
+import ArrBtnPic from "src/components/navigation-bar/arrow-button/ArrBtnPic/ArrBtnPic"
 require('./PopularTopics.scss')
 
 class PopularTopics extends React.Component{
@@ -11,16 +12,21 @@ class PopularTopics extends React.Component{
       <div className="PopularTopicStyle">
         <PopularTopicsTopBar/>
       
+        <div className="wrapper_for_contentTopics">
           <div className="ContentTopics_list">
             <ContentTopics/>
             <ContentTopics/>
             <ContentTopics/>
+            <ContentTopics/>
+            
           </div>
 
           <div className="button-scroll">
-          sdds
+            {/* <ArrBtnPic/> */}
+            
           </div>
-        </div> 
+        </div>
+      </div> 
     )
   }
 }
