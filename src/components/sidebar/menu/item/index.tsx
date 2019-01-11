14f10eac07840a9e748582eacfd9742b1f470ccc
AlexeyArno <ledssssa@gmail.com>
2019-01-11 20:58:41 +0300

    Done sidebar

--- src/components/sidebar/menu/item/index.tsx
+++ src/components/sidebar/menu/item/index.tsx
@@ -1,27 +1,33 @@
 import * as React from "react";
-import { observer, inject } from "mobx-react";
+import { Link } from "react-router-dom";
 require("./styles.scss");
 
 interface ISideBarMenuItemProps {
   icon: string;
   name: string;
   active: boolean;
-  click: (name: string) => void;
   open: boolean;
 }
 
-@inject("store")
-@observer
 export default class SideBarMenuItem extends React.Component<ISideBarMenuItemProps> {
   constructor(props) {
     super(props);
   }
+
   public render() {
+    const wrapCN: string = this.props.open ? "sidebar-item-open" : "sidebar-item-close";
+    let iconCN: string = this.props.open ? "sidebar-item__icon" : "sidebar-item__icon-close";
+    iconCN += (this.props.active ? "-active" : "");
+    const nameCN: string = this.props.open ? "sidebar-item__name-open" : "sidebar-item__name-close";
     return(
-      <div>
-        <div dangerouslySetInnerHTML={{__html: this.props.icon}}/>
-        <div>{this.props.name}</div>
-      </div>
+      <Link  className={wrapCN} to={"/" + this.props.name} >
+        <div
+          dangerouslySetInnerHTML={{__html: this.props.icon}}
+          className={iconCN}
+        />
+        <div className={nameCN}>{this.props.name}</div>
+
+      </Link>
     );
   }
 }
