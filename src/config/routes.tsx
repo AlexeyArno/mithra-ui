8284b9b1f6faf735a9ce8883580ef5ad3e79f4fb
Pestartz <iskanderbakhvalov1@gmail.com>
2019-01-12 11:51:22 +0300

    added new screen

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,14 +1,14 @@
 import PopularPage from "../pages/popular";
 // import NavigationBar from "../components/navigation-bar";
 import SideBar from "../components/sidebar";
-import SignUp from "../pages/sign-up/index";
+import SignIn from "../pages/auth-windows/sign-up/index";
 import {IRoute} from "src/interfaces/routes";
 
 import * as React from "react";
 
 const routes: {[key: string]: IRoute} = {
   // Popular: {path: "/popular", component: <div className="coupleWrapper"><SideBar/> <PopularPage/></div>},
-  Popular: {path: "/popular", component: <div className="coupleWrapper"> <SignUp/><div className="content"/></div>},
+  Popular: {path: "/popular", component: <div className="coupleWrapper"> <SignIn/><div className="content"/></div>},
 };
 
 export default routes;
